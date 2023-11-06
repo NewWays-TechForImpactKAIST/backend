9571c7d98935600be8d8d2d873c7002fa1d81f8a
songc04 <songmhrm04@gmail.com>
2023-11-06 18:41:58 +0900

    feat: mongoose test (not done)

--- src/api/test.ts
+++ src/api/test.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import {Scrap} from "db/mongo";
 
 const router: express.Router = express.Router();
 
@@ -6,8 +7,10 @@ router.get('/', (req: express.Request, res: express.Response)=>{
     return res.send('test route');
 });
 
-router.get('/test', (req: express.Request, res: express.Response)=>{
-    return res.send('test/test route');
+router.get('/mongo', (req: express.Request, res: express.Response)=>{
+    Scrap.find({}).then((data)=>{
+        res.send(data);
+    })
 });
 
 export default router;
