e8e82a66779fe4338ad2669e469c4bce9003366f
happycastle <41810556+happycastle114@users.noreply.github.com>
2023-11-08 20:52:51 +0900

    Add: Scrap Diversity Data

--- src/controllers/scrapResult.ts
+++ src/controllers/scrapResult.ts
@@ -10,3 +10,19 @@ export const getScrapResult = async (req: Request, res: Response) => {
     res.status(500).send("scrapResult/getScrapResult: Internal Server Error");
   }
 };
+
+export const getDiversity = async (req: Request, res: Response) => {
+  try {
+    const scrapResults = await ScrapResultModel.find({});
+    const diversity = scrapResults.map(scrapResult => {
+      return {
+        name: scrapResult.name,
+        diversity: scrapResult.diversity,
+      };
+    });
+    res.status(200).send(diversity);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("scrapResult/getDiversity: Internal Server Error");
+  }
+};
